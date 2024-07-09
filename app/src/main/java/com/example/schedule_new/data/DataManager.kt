5da6e5c9d245fb5031def2894134eec36d206f9b
mitrich772 <mitrichdmitrich772@gmail.com>
2024-07-09 19:42:23 +0300

    dropdown

--- app/src/main/java/com/example/schedule_new/data/DataManager.kt
+++ app/src/main/java/com/example/schedule_new/data/DataManager.kt
@@ -6,16 +6,24 @@ object DataManager {
     var json : String = ""
     var groupNumber = null
     val gson = Gson()
-    fun takeJsonFromServer(){
+    fun takeJsonFromServerByWeekNumber(weekNumber: Int){
         json = "{\"number\": 1, \"days\": [{\"name\": \"Пт\", \"date\": \"09.02.24\", \"lessons\": [{\"name\": \"Общая физика \", \"place\": \"ГУК Б-444\", \"teacher\": \"Побережский Сергей Юрьевич\", \"time\": \"18:15 – 19:45\", \"type\": \"ЛК\"}, {\"name\": \"Общая физика \", \"place\": \"ГУК Б-444\", \"teacher\": \"Побережский Сергей Юрьевич\", \"time\": \"20:00 – 21:30\", \"type\": \"ЛК\"}]},{\"name\": \"Вт\", \"date\": \"09.02.24\", \"lessons\": [{\"name\": \"Общая физика \", \"place\": \"ГУК Б-444\", \"teacher\": \"Побережский Сергей Юрьевич\", \"time\": \"18:15 – 19:45\", \"type\": \"ЛК\"}, {\"name\": \"Общая физика \", \"place\": \"ГУК Б-444\", \"teacher\": \"Побережский Сергей Юрьевич\", \"time\": \"20:00 – 21:30\", \"type\": \"ЛК\"}]}]}"
-
     }
-    fun nonExamWeekObjectFromJsonString(): Week {
-        takeJsonFromServer()
+    fun nonExamWeekObjectFromJsonString(weekNumber: Int): Week {
+        takeJsonFromServerByWeekNumber(weekNumber)
         return gson.fromJson(json,Week::class.java)
     }
-    fun examsFromJsonString(): Week{
-        takeJsonFromServer()
+    fun examsFromJsonString(weekNumber: Int): Week{
+        takeJsonFromServerByWeekNumber(weekNumber)
         return gson.fromJson(json,Week::class.java)
+    }
+    fun getWeeksNumberFromGroup(group:String) : Int{
+        //запрос
+        return 3
+    }
+    fun listOfWeek(number:Int): MutableList<String> {
+        var start = 0
+        var weekArray = generateSequence { start += 1; "Неделя " + start.toString()}.take(number+1).toMutableList()
+        return weekArray
     }
 }
--- app/src/main/java/com/example/schedule_new/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/schedule_new/ui/schedule/ScheduleFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.schedule_new.R
 import com.example.schedule_new.data.DataManager
 import com.example.schedule_new.data.DayAdapter
 import com.example.schedule_new.databinding.FragmentScheduleBinding
 
-class ScheduleFragment : Fragment() {
+class ScheduleFragment : Fragment(),AdapterView.OnItemSelectedListener {
 
     private var _binding: FragmentScheduleBinding? = null
 
@@ -20,7 +26,7 @@ class ScheduleFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     private val dayAdapterForSchedule = DayAdapter()
-    private var dayListForSchedule = DataManager.nonExamWeekObjectFromJsonString().days
+    private var dayListForSchedule = DataManager.nonExamWeekObjectFromJsonString(0).days
 
 
     override fun onCreateView(
@@ -33,6 +39,17 @@ class ScheduleFragment : Fragment() {
 
         _binding = FragmentScheduleBinding.inflate(layoutInflater)
         val root: View = binding.root
+
+        val spinner: Spinner = binding.spinnerView
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_1,DataManager.listOfWeek(5)).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = this
+        }
+
 
         val textView: TextView = binding.textSchedule
         scheduleViewModel.text.observe(viewLifecycleOwner) {
@@ -53,5 +70,17 @@ class ScheduleFragment : Fragment() {
             rcViewShedule.adapter = dayAdapterForSchedule
             dayAdapterForSchedule.newDaysFromWeek(dayListForSchedule)
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        Toast.makeText(this.context, pos.toString(), Toast.LENGTH_SHORT).show()
+        _binding?.apply {
+            dayListForSchedule = DataManager.nonExamWeekObjectFromJsonString(pos).days
+            dayAdapterForSchedule.newDaysFromWeek(dayListForSchedule)
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
